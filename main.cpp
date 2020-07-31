9e18f8dd84335d6b0585d5f53a0e95dc2a2750d5
Matthew Early <matthewjearly@gmail.com>
2020-07-31 14:01:09 -0400

    glfw demo

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <adder.h>
-
+#include <GLFW/glfw3.h>
 
 int main()
 {
@@ -10,6 +10,33 @@ int main()
 
 
     std::cout << add(72.1f, 73.8f) << '\n';
+
+    GLFWwindow *window;
+    if( !glfwInit() )
+    {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        exit( EXIT_FAILURE );
+    }
+    window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
+    if (!window)
+    {
+        fprintf( stderr, "Failed to open GLFW window\n" );
+        glfwTerminate();
+        exit( EXIT_FAILURE );
+    }
+       // Main loop
+    while( !glfwWindowShouldClose(window) )
+    {
+
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Terminate GLFW
+    glfwTerminate();
+
+
 
 
     return 0;
